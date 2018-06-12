ebb9a02a30f9e411d7a56c10d51d27614a0ae467
Francis <francispham89@gmail.com>
2018-06-12 13:36:42 -0700

    add beforeEach fn

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -5,6 +5,12 @@ import App from '../App';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />)
+});
+
 /*
 it('shows a comment box', () => {
   const div = document.createElement('div');
@@ -20,13 +26,9 @@ it('shows a comment box', () => {
 */
 
 it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentBox).length).toEqual(1);
-})
+});
 
 it('show a comment list', () => {
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentList).length).toEqual(1);
-})
+});
