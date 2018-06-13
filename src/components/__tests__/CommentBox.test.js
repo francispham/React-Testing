5efa6716786a11260b205bc0cb5bd154b585ee1b
Francis <francispham89@gmail.com>
2018-06-13 04:59:15 -0700

    Textarea Test Implementation 2

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -25,3 +25,30 @@ it('has a text area that users can type in', () => {
 
   expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
 })
+
+it('has an emptied textarea when users click Submit', () => {
+  wrapped.find('textarea').simulate('change', {
+    target: { value: 'new comment' }
+  });
+  wrapped.update();
+
+  wrapped.find('form').simulate('submit');
+  wrapped.update();
+  expect(wrapped.find('textarea').prop('value')).toEqual ('')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
