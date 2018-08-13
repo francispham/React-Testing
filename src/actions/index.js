9693e470ce7899f7dba77f69ca2f3b334ff21fb7
Francis <francispham89@gmail.com>
2018-08-13 00:27:25 -0700

    Setup for Fetching a Remote Resource

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,19 @@
-import { SAVE_COMMENT } from 'actions/types';
+import axios from 'axios';
+import { SAVE_COMMENT, FETCH_COMMENT } from 'actions/types';
 
 export function saveComment(comment) {
   return {
     type: SAVE_COMMENT,
     payload: comment
+  };
+}
+
+export function fetchComments() {
+  // Fetching a remote Resource
+  const response = axios.get('http://jsonplaceholder.typicode.com/comments');
+
+  return {
+    type: FETCH_COMMENT,
+    payload: response
   };
 }
