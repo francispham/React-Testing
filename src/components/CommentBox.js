e17d4785a1eeaae33d52597ec18d517bf2870dd2
Francis <francispham89@gmail.com>
2018-08-16 01:29:09 -0700

    Use of this.props.history in React Router Property

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -19,7 +19,7 @@ class CommentBox extends Component {
   // Helper Methods:
   shouldNavigateAway() {
     if (!this.props.auth) {
-      console.log('I NEED TO LEAVE!!!');
+      this.props.history.push('/');
     }
   }
 
