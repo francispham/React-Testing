239d4458ff184cc94cbaccd2060703c7e14fc0b4
Francis <francispham89@gmail.com>
2018-08-15 23:44:03 -0700

    Setup renderButton, renderHeader, function mapStateToProps

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
-export default () => {
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  )
+class App extends Component {
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>
+          SIGN OUT
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={() => this.props.changeAuth(true)}>
+          SIGN IN
+        </button>
+      );
+    }
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">HOME</Link>
+        </li>
+        <li>
+          <Link to="/post">POST A COMMENT</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    )
+  }
 };
+
+function mapStateToProps(state) {
+  return { auth: state.auth }
+}
+export default connect(mapStateToProps, actions)(App);
