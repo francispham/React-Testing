2f79a16df89b46616cb6a6067e0d059f65ab3138
Francis <francispham89@gmail.com>
2018-08-16 21:33:41 -0700

    Finish with dispatch action

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -8,7 +8,18 @@ export default function({ dispatch }) {
 }
 */
 
-// Refactor(WOW!!!):
+// Refactor the above(WOW!!!):
 export default ({ dispatch }) => next =>action => {
+  // Check to see if the action has a promise on its 'payload' property.
 
+  // If it doesn't, then send the action on to the next middleware.
+  if(!action.payload || !action.payload.then) {
+    return next(action);
+  }
+  // If it does, then wait for it to resolve, (get its data!!) and then
+  // create a new action with that data and dispatch it.
+  action.payload.then(function(response) {
+    const newAction = { ...action, payload: response };
+    dispatch(newAction);
+  });
 };
